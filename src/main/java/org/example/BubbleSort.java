c4649f5f5f175182dd824231edfce5dabe77045b
Apg576 <myp0st.f@gmail.com>
2024-02-12 20:07:26 +0500

    Сделал сортировку с дженериками.

--- src/main/java/org/example/BubbleSort.java
+++ src/main/java/org/example/BubbleSort.java
@@ -3,16 +3,16 @@ package org.example;
 /**
  * Сортировка пузырьком
  */
-public class BubbleSort {
+public class BubbleSort<T extends Comparable<T>> {
     /**
      *
-     * @param a Собственно масив для сортировки
+     * @param a Собственно массив для сортировки
      */
-    public void bubbleSort(int[] a){
+    public void bubbleSort(T[] a){
         for (int i=a.length;i>0;i--){
             for (int j=0;j<i-1;j++){
-                if (a[j]>a[j+1]){
-                    int m=a[j+1];a[j+1]=a[j];a[j]=m;
+                if (a[j].compareTo(a[j+1])>0){
+                    T m=a[j+1];a[j+1]=a[j];a[j]=m;
                 }
             }
         }
--- src/test/java/org/example/BubbleSortTest.java
+++ src/test/java/org/example/BubbleSortTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,21 +11,18 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BubbleSortTest {
-    private int[] a, mix;
+    private Integer[] a, mix;
     private final int COUNT = 20;
     @Before
     public void init(){
         // формируем упорядоченный по возрастанию массив - то что должно в итоге получиться
-        a = new int[COUNT];   mix = new int[COUNT];
+        a = new Integer[COUNT];   mix = new Integer[COUNT];
         ArrayList<Integer> list = new ArrayList<>();
         Random random = new Random();
         for (int i=0;i<COUNT;i++){
             a[i]=random.nextInt(i*10,10*(i+1));
             list.add(a[i]);
         }
-        System.out.println("Начальный массив:");
-        Arrays.stream(a).forEach(el -> System.out.print(el+" "));
-        System.out.println();
         // Перемешиваем элементы массива
         for (int i=0;i<COUNT;i++){
             int m = random.nextInt(list.size());
@@ -31,14 +30,28 @@ public class BubbleSortTest {
         }
     }
     @Test
-    public void testBubbleSort(){
-        System.out.println("Подготовленный для сортировки массив:");
-        Arrays.stream(mix).forEach(el -> System.out.print(el+" "));
+    public void testWithInteger(){
+        testBubbleSort(a,mix);
+    }
+    @Test
+    public void testWithString(){
+        var orig = new String[]{"a","b","c","d"};
+        testBubbleSort(orig,new String[]{"d","a","c","b"});
+    }
+
+
+    public <T extends Comparable<T>> void testBubbleSort(T[] orig,T[] mixed){
+        System.out.println("Начальный массив:");
+        Arrays.stream(orig).forEach(el -> System.out.print(el+" "));
         System.out.println();
-        new BubbleSort().bubbleSort(mix);
+        System.out.println("Подготовленный для сортировки массив:");
+        Arrays.stream(mixed).forEach(el -> System.out.print(el+" "));
+        System.out.println();
+        new BubbleSort().bubbleSort(mixed);
         System.out.println("Отсортированный массив:");
-        Arrays.stream(mix).forEach(el -> System.out.print(el+" "));
-        assertArrayEquals(a,mix);
+        Arrays.stream(mixed).forEach(el -> System.out.print(el+" "));
+        System.out.println();
+        assertArrayEquals(orig,mixed);
     }
 
 }
