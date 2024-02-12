f192171d8dc774d4b3aa9e90ccfbdd0c16ec7286
Apg576 <myp0st.f@gmail.com>
2024-02-12 20:41:44 +0500

    Исправил тесты.

--- src/test/java/org/example/BubbleSortTest.java
+++ src/test/java/org/example/BubbleSortTest.java
@@ -1,52 +1,52 @@
 package org.example;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BubbleSortTest {
-    private Integer[] a, mix;
-    private final int COUNT = 20;
-    @Before
-    public void init(){
-        // формируем упорядоченный по возрастанию массив - то что должно в итоге получиться
-        a = new Integer[COUNT];   mix = new Integer[COUNT];
-        ArrayList<Integer> list = new ArrayList<>();
+    @Test
+    public void testWithInteger(){
+        final int COUNT = 20;
+        // формируем упорядоченный по возрастанию массив
+        Integer[] a = new Integer[COUNT];
         Random random = new Random();
         for (int i=0;i<COUNT;i++){
             a[i]=random.nextInt(i*10,10*(i+1));
-            list.add(a[i]);
         }
-        // Перемешиваем элементы массива
-        for (int i=0;i<COUNT;i++){
-            int m = random.nextInt(list.size());
-            mix[i] = list.get(m); list.remove(m);
-        }
-    }
-    @Test
-    public void testWithInteger(){
-        testBubbleSort(a,mix);
+        testBubbleSort(a);
     }
     @Test
     public void testWithString(){
         var orig = new String[]{"a","b","c","d"};
-        testBubbleSort(orig,new String[]{"d","a","c","b"});
+        testBubbleSort(orig);
+    }
+    @Test
+    public void testWithCharacters(){
+        var orig = new Character[]{'a','b','c','d'};
+        testBubbleSort(orig);
     }
 
-
-    public <T extends Comparable<T>> void testBubbleSort(T[] orig,T[] mixed){
-        System.out.println("Начальный массив:");
+    public <T extends Comparable<T>> void testBubbleSort(T[] orig){
+        var forClass =orig[0];
+        System.out.println("Начальный массив "+forClass.getClass().getName()+" :");
         Arrays.stream(orig).forEach(el -> System.out.print(el+" "));
         System.out.println();
+        // Перемешиваем элементы массива
+        Random random = new Random();
+        ArrayList<T> list = new ArrayList<>();
+        for (int i=0;i<orig.length;i++) list.add(orig[i]);
+        T[] mixed = Arrays.copyOf(orig,orig.length);
+        for (int i=0;i<orig.length;i++){
+            int m = random.nextInt(list.size());
+            mixed[i] = list.get(m); list.remove(m);
+        }
         System.out.println("Подготовленный для сортировки массив:");
         Arrays.stream(mixed).forEach(el -> System.out.print(el+" "));
         System.out.println();
+
         new BubbleSort().bubbleSort(mixed);
         System.out.println("Отсортированный массив:");
         Arrays.stream(mixed).forEach(el -> System.out.print(el+" "));
